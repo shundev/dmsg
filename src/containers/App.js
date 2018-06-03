914ca18c4f90665767c2d33c009fdd3c6da5bd5e
hondo-s <hondo-s@klab.com>
2018-06-03 10:28:13 +0900

    remove all

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,52 +1,20 @@
 import 'babel-polyfill'
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { createStore } from 'redux'
 
 import { store } from '../store'
-import ChatInput from '../components/ChatInput'
-import ChatHistory from '../components/ChatHistory'
-import ChangeFriend from '../components/ChangeFriend'
 import * as actionTypes from '../constants/actionTypes'
 
 class App extends Component {
-  componentWillMount() {
-      store.dispatch({ type: actionTypes.FETCH_WEB3_CONNECTION_REQUESTED })
-  }
-
-  componentDidUpdate() {
-      this.scrollDown()
-  }
-
   render() {
-    const { userAddress, friendAddress, history, fetchHistory, changeFriend, sendMessage } = this.props
     return (
       <div className="App">
-        <ChangeFriend friendAddress={ friendAddress } changeFriend={ changeFriend } />
-        <ChatHistory history={ history } fetchHistory={ fetchHistory }/>
-        <ChatInput userAddress={ userAddress } friendAddress={ friendAddress } sendMessage={ sendMessage } />
+        <h1>Hello, dApps!</h1>
       </div>
     );
   }
-
-  scrollDown() {
-      window.scrollTo(0, document.body.scrollHeight);
-  }
-
-}
-
-function mapDispatchToProps (dispatch, ownProps) {
-    return {
-        fetchHistory: () => dispatch({ type: actionTypes.FETCH_HISTORY_REQUESTED }),
-        sendMessage: (msg) => dispatch({ type: actionTypes.SEND_MESSAGE_REQUESTED, payload: msg }),
-        changeFriend: (address) => {
-            dispatch({ type: actionTypes.SET_FRIEND_ADDRESS, payload: address })
-            dispatch({ type: actionTypes.FETCH_HISTORY_REQUESTED })
-        },
-    }
 }
 
 export default connect(
     state => state,
-    mapDispatchToProps
 )(App);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -13,31 +13,5 @@ const INITIAL_STATE = {
 
 export default function dmsgReducer(state = INITIAL_STATE, action = {})
 {
-    switch (action.type)
-    {
-        case actionTypes.ADD_MESSAGE:
-            return Object.assign({}, state, {
-                history: state.history.concat(action.payload)
-            })
-        case actionTypes.SET_USER_ADDRESS:
-            return Object.assign({}, state, {
-                userAddress: action.payload
-            })
-        case actionTypes.SET_FRIEND_ADDRESS:
-            return Object.assign({}, state, {
-                friendAddress: action.payload
-            })
-        case actionTypes.FETCH_WEB3_CONNECTION_SUCCESS:
-            return Object.assign({}, state, {
-                web3: action.web3,
-                userAddress: action.userAddress,
-                contractInstance: action.contractInstance
-            })
-        case actionTypes.FETCH_HISTORY_SUCCESS:
-            return Object.assign({}, state, {
-                history: action.payload
-            })
-        default:
-            return state
-    }
+    return state
 }
